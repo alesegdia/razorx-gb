dcbf3ba0c6e74442b8a237da44a39fc586520a17
Alejandro Seguí <alesegdia@gmail.com>
2016-07-03 01:39:22 +0200

    adding another brush

--- src/game/painter.h
+++ src/game/painter.h
@@ -5,18 +5,21 @@
 
 typedef struct
 {
-	brush_t brushes[1];
+	brush_t brushes[2];
 } painter_t;
 
 void painter_initialize( painter_t* painter )
 {
 	brush_initialize( painter->brushes[0], 1, 0x0300 );
+	brush_initialize( painter->brushes[1], 0, 0x1000 );
 }
 
 void painter_scroll( painter_t* painter, UBYTE ypos )
 {
 	brush_linear_translation( painter->brushes );
 	brush_paint( painter->brushes, ypos );
+	brush_linear_translation( (&(painter->brushes[1])) );
+	brush_paint( (&(painter->brushes[1])), ypos );
 }
 
 #endif
