e0bdf8858321b5032e70f35bb7c2bd9b68f84348
Alejandro Seguí <alesegdia@gmail.com>
2016-07-03 03:49:35 +0200

    using screen to update gameplay

--- src/razorx.c
+++ src/razorx.c
@@ -5,8 +5,11 @@
 #include <gb/gb.h>
 
 #include "game/gameplay.h"
+#include "game/screen.h"
 
 UBYTE joypad_state;
+
+screen_t gameplay_scrn;
 
 // to be able to use WORD, we need to declare it
 // outside the main function
@@ -14,22 +17,14 @@ UBYTE joypad_state;
 
 void main()
 {
-	disable_interrupts();
-	DISPLAY_OFF;
-	SPRITES_8x8;
+	gameplay_scrn.loop_callback = gameplay;
+	gameplay_scrn.init_callback = game_start;
 
-	game_start();
-
-	SHOW_BKG;
-	SHOW_SPRITES;
-	DISPLAY_ON;
-
-	enable_interrupts();
+	screen_init( &gameplay_scrn );
 
 	while(1)
 	{
 		wait_vbl_done();
-		joypad_state = joypad();
-		gameplay(joypad_state);
+		screen_loop( &gameplay_scrn );
 	}
 }
