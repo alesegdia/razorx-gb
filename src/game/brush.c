04a5d3fecec01f963a8f0740c40d12786f6ae9ab
Alejandro Seguí <alesegdia@gmail.com>
2016-07-03 03:43:54 +0200

    moving brush definitions to .c

--- src/game/brush.c
+++ src/game/brush.c
@@ -1,5 +1,13 @@
 #include "brush.h"
 
+void brush_paint( brush_t* brush, UBYTE ypos )
+{
+	UBYTE free_tile = 0;
+	set_bkg_tiles( brush->x.b.h, ypos, 1, 1, &free_tile );
+	set_bkg_tiles( brush->x.b.h+1, ypos, 1, 1, &free_tile );
+	set_bkg_tiles( brush->x.b.h-1, ypos, 1, 1, &free_tile );
+}
+
 void brush_linear_translation( brush_t* brush )
 {
 	if( brush->sign == 0 )
--- src/game/brush.h
+++ src/game/brush.h
@@ -14,13 +14,7 @@ typedef struct
 	b.x.w = v
 
 void brush_linear_translation( brush_t* brush );
+void brush_paint( brush_t* brush, UBYTE ypos );
 
-void brush_paint( brush_t* brush, UBYTE ypos )
-{
-	UBYTE free_tile = 0;
-	set_bkg_tiles( brush->x.b.h, ypos, 1, 1, &free_tile );
-	set_bkg_tiles( brush->x.b.h+1, ypos, 1, 1, &free_tile );
-	set_bkg_tiles( brush->x.b.h-1, ypos, 1, 1, &free_tile );
-}
 
 #endif
