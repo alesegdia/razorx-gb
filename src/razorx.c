adbf05f22b2da02c700ee2d6f63c778e4b29d87d
Alejandro Seguí <alesegdia@gmail.com>
2016-07-04 01:27:33 +0200

    working on splash screen and generic screen loop

--- src/razorx.c
+++ src/razorx.c
@@ -5,26 +5,32 @@
 #include <gb/gb.h>
 
 #include "game/gameplay.h"
+#include "game/splash.h"
 #include "game/screen.h"
 
 UBYTE joypad_state;
 
 screen_t gameplay_scrn;
+screen_t splash_scrn;
+
 
 // to be able to use WORD, we need to declare it
 // outside the main function
 
-
 void main()
 {
-	gameplay_scrn.loop_callback = gameplay;
 	gameplay_scrn.init_callback = game_start;
+	gameplay_scrn.loop_callback = gameplay;
 
-	screen_init( &gameplay_scrn );
+	splash_scrn.init_callback = splash_start;
+	splash_scrn.loop_callback = splash_step;
+
+	screen_change( &gameplay_scrn );
 
 	while(1)
 	{
 		wait_vbl_done();
 		screen_loop( &gameplay_scrn );
+		//screen_loop( current_screen );
 	}
 }
