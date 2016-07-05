f091964cfc10df670f52a1101dccf006eebe6b28
Alejandro Seguí <alesegdia@gmail.com>
2016-07-06 01:59:32 +0200

    info text at splash

--- src/game/splash.c
+++ src/game/splash.c
@@ -8,9 +8,17 @@ void splash_start()
 
 }
 
+UBYTE b = 0;
+
 UBYTE splash_step()
 {
 	UBYTE joypad_state = joypad();
+	if( !b )
+	{
+		b = 1;
+		printf("Press A to start.\n");
+	}
+
 	if( joypad_state & J_A )
 	{
 		return 2;
